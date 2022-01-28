8616a99aa9b8b8c4c480ee69814fcc238c2b3ecd
Pedro Brant <pkb@cin.ufpe.br>
2022-01-27 21:11:08 -0300

    ADD corte and reset buttons

--- telaNovas/telaNovas.ino
+++ telaNovas/telaNovas.ino
@@ -11,8 +11,6 @@
 #define P3_PRENSOR_SAIDA       32
 #define P4_PUXADOR             04
 
-
-
 #define A1_RETORNO_CARRINHO    15
 #define A2_AVANCO_SERRA        14
 #define A3_RETORNO_SERRA       05
@@ -34,6 +32,9 @@ AccelStepper stepper(AccelStepper::DRIVER, M1_STEP, M1_DIRECTION);
 
 uint16_t IHM_STA_Pagina = 0;
 int16_t IHM_BTN_Luz = 0;
+
+int16_t comando[17]= {0};
+int16_t comando_manual[6] = {0};
 int16_t saidas[8] = {0};
 int16_t entradas[8] = {0};
 
@@ -76,6 +77,7 @@ void loop() {
   select_screen(IHM_STA_Pagina);
   
 }
+
 void select_screen(uint16_t IHM_STA_Pagina){
   switch(IHM_STA_Pagina){
     case 0:
@@ -84,38 +86,97 @@ void select_screen(uint16_t IHM_STA_Pagina){
       
      case 1:
       Serial.println("Operador Automático");
-      zera_saidas();
+      AUTOMATICO_read_comando();
+      TESTE_zera_saidas();
       break;
       
      case 5:
       Serial.println("Operador Manual");
-      zera_saidas();
+      Serial.println(comando_manual[23]);
+      MANUAL_read_comando();
+      MANUAL_corte();
+      MANUAL_reset();
+      MANUAL_zera_botoes();
+      TESTE_zera_saidas();
       break;
       
      case 7:
       Serial.println("Modo Testes");
-      read_saidas();
-      write_saidas();
-      read_entradas();
+      TESTE_read_saidas();
+      TESTE_write_saidas();
+      TESTE_read_entradas();
       break;
   }
 }
-void zera_saidas(){
+
+//------------------------------------------
+// OPERADOR AUTOMATICO
+
+void AUTOMATICO_read_comando(){
+  for(int i=1;i<18;i++){
+    Serial.print(comando[i]);
+      Comm3.read(i, &comando[i]);
+  }
+}
+
+void AUTOMATICO_retorna_carrinho(){
+  Serial.print("Voltando");
+  stepper.move(-1000000);// TODO CALCULAR DISTANCIA
+  stepper.enableOutputs();
+  while(digitalRead(A1_RETORNO_CARRINHO)){
+    digitalRead(A1_RETORNO_CARRINHO);
+    stepper.run();
+  }
+  stepper.disableOutputs();
+  Serial.print("Retornei para o início");
+}
+//------------------------------------------
+// OPERADOR MANUAL
+
+void MANUAL_read_comando(){
+  for(uint16_t i=18;i<24;i++){
+      Comm3.read(i, &comando_manual[i-18]);
+      Serial.print(comando_manual[i-18]);
+  } 
+}
+
+void MANUAL_zera_botoes(){
+  for(uint16_t i=20;i<24;i++){
+      Comm3.write(i, 0);
+  } 
+}
+void MANUAL_corte(){
+  if(comando_manual[4] == 1){
+    if(digitalRead(A2_AVANCO_SERRA)|| true)CORTE_AVANCO();
+    delay(3000);//teste
+    if(digitalRead(A3_RETORNO_SERRA)|| true)CORTE_RETORNO();
+  }
+}
+
+void MANUAL_reset(){// Volta para o início
+  if(comando_manual[5] == 1){
+    AUTOMATICO_retorna_carrinho();
+  }
+}
+
+
+//------------------------------------------
+// MODO DE TESTES
+
+void TESTE_zera_saidas(){
   for(int i=24;i<32;i++){
     Comm3.write(i, 0);
   }
 }
 
-void read_saidas(){
+void TESTE_read_saidas(){
   for(uint16_t i=24;i<32;i++){
       Comm3.read(i, &saidas[i-24]);
   }
 }
 
-void write_saidas(){
-  Serial.println(10*(1-2*saidas[1]));
+void TESTE_write_saidas(){
   if(saidas[0]==1) TESTE_moveDistancia(100);
-  if(saidas[1]==1)
   if(saidas[2]==1)       stepper.enableOutputs();
   else if(saidas[2]==0) stepper.disableOutputs();
   //TODO adicionar para o motor de passo
@@ -126,7 +187,7 @@ void write_saidas(){
   digitalWrite(P4_PUXADOR,         saidas[7]);
 }
 
-void read_entradas(){
+void TESTE_read_entradas(){
   entradas[0] = digitalRead(A1_RETORNO_CARRINHO);
   entradas[1] =     digitalRead(A2_AVANCO_SERRA);
   entradas[2] =    digitalRead(A3_RETORNO_SERRA);
@@ -137,7 +198,6 @@ void read_entradas(){
   entradas[7] =          digitalRead(A8_RESERVA);
   
   for(int i=32;i<40;i++){
-    Serial.print(entradas[i-32]);
     Comm3.write(i, entradas[i-32]);
   }
 }
@@ -151,6 +211,79 @@ void TESTE_moveDistancia(float distancia) {
     Comm3.read(24, &saidas[0]);
     Comm3.read(25, &saidas[1]);
     Comm3.read(26, &saidas[2]);
-    stepper.run();}
+    stepper.run();
+  }
   stepper.disableOutputs();
+}
+
+//------------------------------------------
+// MAQUINA DE ESTADOS
+
+void ESTADO_INICIAL(){ 
+  Serial.println("ESTADO_INICIAL");
+        
+  digitalWrite(M1_STEP, LOW);
+  digitalWrite(M1_DIRECTION, LOW);
+  digitalWrite(M2_SERRA, LOW);
+  digitalWrite(P1_PRENSOR_ENTRADA, LOW);
+  digitalWrite(P2_CARRINHO_SERRA, LOW);
+  digitalWrite(P3_PRENSOR_SAIDA, LOW);
+  digitalWrite(P4_PUXADOR, LOW);
+}
+
+void EMPURRA_MADEIRA(float distancia){// TODO adicionar e calibrar avanço do motor de passo em cm
+  Serial.print("EMPURRA_MADEIRA ");
+  Serial.println(distancia);
+      
+  digitalWrite(M1_STEP, LOW);
+  digitalWrite(M1_DIRECTION, HIGH);
+  digitalWrite(M2_SERRA, LOW);
+  digitalWrite(P1_PRENSOR_ENTRADA, LOW);
+  digitalWrite(P2_CARRINHO_SERRA, LOW);
+  digitalWrite(P3_PRENSOR_SAIDA, LOW);
+  digitalWrite(P4_PUXADOR, LOW);
+
+  //moveDistancia(distancia);
+}
+
+void CORTE_AVANCO(){ 
+  Serial.println("CORTE_AVANCO ");
+        
+  digitalWrite(M1_STEP, LOW);
+  digitalWrite(M1_DIRECTION, LOW);
+  digitalWrite(M2_SERRA, HIGH);
+  digitalWrite(P1_PRENSOR_ENTRADA, HIGH);
+  digitalWrite(P2_CARRINHO_SERRA, HIGH);
+  digitalWrite(P3_PRENSOR_SAIDA, HIGH);
+  digitalWrite(P4_PUXADOR, LOW);     
+}
+
+void CORTE_RETORNO(){// TODO serra ligada na volta?
+  Serial.println("CORTE_RETORNO ");
+        
+  digitalWrite(M1_STEP, LOW);
+  digitalWrite(M1_DIRECTION, LOW);
+  digitalWrite(M2_SERRA, LOW);
+  digitalWrite(P1_PRENSOR_ENTRADA, HIGH);
+  digitalWrite(P2_CARRINHO_SERRA, LOW);
+  digitalWrite(P3_PRENSOR_SAIDA, HIGH);
+  digitalWrite(P4_PUXADOR, HIGH);     
+}
+
+void RETORNO(){// TODO adicionar motor de passo
+  Serial.println("RETORNO ");
+        
+  digitalWrite(M1_STEP, LOW);
+  digitalWrite(M1_DIRECTION, LOW);
+  digitalWrite(M2_SERRA, LOW);
+  digitalWrite(P1_PRENSOR_ENTRADA, LOW);
+  digitalWrite(P2_CARRINHO_SERRA, LOW);
+  digitalWrite(P3_PRENSOR_SAIDA, LOW);
+  digitalWrite(P4_PUXADOR, LOW); 
+  
+  //retorna_carrinho();
+}
+
+void HALT_STOP(){// TODO adicionar motor de passo
+  Serial.println("HALT ");
 }
